10fa542b5e9b08397b885584c7154ab74dcc6379
alioshja <laboucheriedu12cestbeau@gmail.com>
2023-12-06 09:28:02 +0100

    TRT-conseil 06/12/23  9H30

--- php/connection-pdo.php
+++ php/connection-pdo.php
@@ -8,11 +8,10 @@ $password = 'jg8knnts6wxd9va1';
 $database = 'nfyz4op4dl8fjihn';
 
 try {
-$conect = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+$conect = new PDO("mysql:host=$hostname;port=3306;dbname=$database", $username, $password);
 $conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
 echo 'une erreur est survenu lors de la connection a la base de donées' . $e->getMessage();
 }
-// mysql -h wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com -u c8fhliske9eu83e9 -pzf2o6e20su8f3pwy fbo68nburz9od6in < \sql\initialisation-Bdd.sql
 ?>
