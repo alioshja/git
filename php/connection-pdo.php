820b27b222231559871a0af3a5fdb9683a835b3c
alioshja <laboucheriedu12cestbeau@gmail.com>
2023-12-27 15:08:18 +0100

    TRT-conseil

--- php/connection-pdo.php
+++ php/connection-pdo.php
@@ -6,9 +6,10 @@ $hostname = 'rwo5jst0d7dgy0ri.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $username = 'ynosn8mgsbydc4qz';
 $password = 'jg8knnts6wxd9va1';
 $database = 'nfyz4op4dl8fjihn';
+$port = '3306';
 
 try {
-$conect = new PDO("mysql://ynosn8mgsbydc4qz:jg8knnts6wxd9va1@rwo5jst0d7dgy0ri.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nfyz4op4dl8fjihn");
+$conect = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
 $conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
